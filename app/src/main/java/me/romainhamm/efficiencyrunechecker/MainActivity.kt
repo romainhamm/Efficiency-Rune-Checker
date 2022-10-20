ab969c6b9335f41f02a81fc9ce9e86866abf5a8b
Romain Hamm <romain.hamm@gmail.com>
2022-10-20 13:55:53 +0200

    add first lib

--- app/src/main/java/me/romainhamm/efficiencyrunechecker/MainActivity.kt
+++ app/src/main/java/me/romainhamm/efficiencyrunechecker/MainActivity.kt
@@ -2,10 +2,16 @@ package me.romainhamm.efficiencyrunechecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hoc081098.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import me.romainhamm.efficiencyrunechecker.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
