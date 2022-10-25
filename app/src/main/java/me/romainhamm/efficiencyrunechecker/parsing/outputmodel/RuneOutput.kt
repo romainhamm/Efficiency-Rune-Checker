b7237996d0f842310ce73f9a93383724f4e61651
Romain Hamm <romain.hamm@gmail.com>
2022-10-25 20:27:59 +0200

    remove some comments

--- app/src/main/java/me/romainhamm/efficiencyrunechecker/parsing/outputmodel/RuneOutput.kt
+++ app/src/main/java/me/romainhamm/efficiencyrunechecker/parsing/outputmodel/RuneOutput.kt
@@ -19,27 +19,3 @@ data class RuneOutput(
     @Json(name = "extra") val baseRank: Int,
     @Json(name = "class") val stars: Int
 ) : Parcelable
-
-/**
-"rune_id": 26338494183,
-"wizard_id": 13183017, // summoner id
-"occupied_type": 2,
-"occupied_id": 0,
-"slot_no": 1,
-"rank": 5,
-"set_id": 1,
-"upgrade_limit": 15,
-"upgrade_curr": 15,
-"base_value": 361380,
-"sell_value": 27019,
-"pri_eff": [ 3, 160 ], // 3 is atk flat, 160 is value
-"prefix_eff": [ 0, 0 ], no innate
-"sec_eff": [
-[ 10, 11, 0, 0 ], // First number is effect so 10 = crit dmg, second one is base, third is and last is grind bonus
-[ 4, 8, 0, 6 ],
-[ 8, 17, 0, 3 ],
-[ 2, 4, 1, 0 ]
-],
-"extra": 4
-}
- **/
